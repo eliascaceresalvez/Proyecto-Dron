fa208346f97f836c473c4810155d1de52dbd42ee
Elías Cáceres <91036133+eliascaceresalvez@users.noreply.github.com>
2022-04-18 15:00:38 -0300

    Initial commit

---
+++ sitio_web.php
@@ -0,0 +1,21 @@
+<?php
+
+  $apellido=$_POST["Your Name"];
+  $nombre=$_POST["Email"];
+  $DNI=$_POST["Phone Number"];
+  $FechaDeNacimiento=$_POST["Message"];
+  $Sexo=$_POST["boton"];
+  
+  echo $apellido;
+  echo "<BR>";
+  echo $nombre;
+  echo "<BR>";
+  echo $DNI;
+  echo "<BR>";
+  echo $FechaDeNacimiento;
+  echo "<BR>";
+  echo $Sexo;
+
+
+
+?>
